02a700c8569d85abc12c712100a2e3e649c85513
RINGDAL Reuben <rringdal@one.steria.dom>
2018-11-30 08:52:09 +0100

    Add navlinks style

--- frontend/src/header/Navigasjon.jsx
+++ frontend/src/header/Navigasjon.jsx
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import NavigasjonLenke from './NavigasjonLenke';
 
-export const Navigasjon = ({username}) => {
+export class Navigasjon extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      showLinks: true
+    }
+    this.toggleLinks = this.toggleLinks.bind(this);
+  }
+  toggleLinks(){
+    this.setState({
+      showLinks: !this.state.showLinks
+    })
+  }
+  render(){
+    const { username } = this.props;
     return (
-      <ul className='header-room-list'>
-        <NavigasjonLenke tekst='Gå til rom 1' link={`/page-one?username=${username}`} />
-        <NavigasjonLenke tekst='Gå til rom 2' link={`/page-two?username=${username}`} />
-        <NavigasjonLenke tekst='Gå til rom 3' link={`/page-three?username=${username}`} />
-        <NavigasjonLenke tekst='Gå til rom 4' link={`/page-four?username=${username}`} />
-        <NavigasjonLenke tekst='Gå til rom 5' link={`/page-five?username=${username}`} />
-      </ul>
+      <div className='room-link-container'>
+        {this.state.showLinks ? <ul className='header-room-list'>
+          <NavigasjonLenke tekst='Gå til rom 1' link={`/page-one?username=${username}`} />
+          <NavigasjonLenke tekst='Gå til rom 2' link={`/page-two?username=${username}`} />
+          <NavigasjonLenke tekst='Gå til rom 3' link={`/page-three?username=${username}`} />
+          <NavigasjonLenke tekst='Gå til rom 4' link={`/page-four?username=${username}`} />
+          <NavigasjonLenke tekst='Gå til rom 5' link={`/page-five?username=${username}`} />
+        </ul>: null}
+        <div className='link-toggler' onClick={this.toggleLinks}>
+          {this.state.showLinks ? <p>Vis</p> : <p>Lukk</p> }
+        </div>
+      </div>
     );
+  }  
+
 }
 
 export default Navigasjon;
--- frontend/src/page-one/PageOne.jsx
+++ frontend/src/page-one/PageOne.jsx
@@ -1,13 +1,7 @@
 import React, { Component } from 'react';
 import './pageOne.css';
-import {Letter} from './Letter';
-import { letters } from './letters'
-import { InputAndButton } from '../commen/InputAndButton';
 import queryString from 'query-string';
-import { apiCallPost, apiCallGet } from '../utils';
-import { NesteOppgaveLenke } from '../commen/NesteOppgaveLenke';
 import { CheckUsername } from '../commen/CheckUsername';
-import Vault from '../content/bank_vault_door.jpg'
 
 export class PageOne extends Component {
   constructor(props){
