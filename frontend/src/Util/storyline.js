0c038258d297db9d24244afb861def0643210fc6
RINGDAL Reuben <rringdal@one.steria.dom>
2018-12-03 19:10:34 +0100

    rom 4 extra

--- frontend/src/Util/storyline.js
+++ frontend/src/Util/storyline.js
@@ -1,8 +1,8 @@
 export const storyline = {
   0: 'Info page',
-  1: 'Dere har kommet dere inn til velvet. Her står dere ovenfor en stor og tung dør dere må prøve å bryte opp for å komme dere videre inn til gullbarene. Det er flere dører i hvelvet som leder til forskjellige rom. En informant dere kontaktet på forhånd har fortalt at hvelvet er sikret med en ny teknologi og er umulig å sprenge. Dere må finne en måte å komme dere gjennom døren med kløkt',
+  1: 'Dere har kommet dere inn til velvet. Her står dere ovenfor en stor og tung dør dere må prøve å åpne opp for å komme dere videre inn til gullbarene. Det er flere dører i hvelvet som leder til forskjellige rom. En informant dere kontaktet på forhånd har fortalt at hvelvet er sikret med en ny teknologi og er umulig å sprenge. Dere må finne en måte å komme dere gjennom døren med kløkt',
   2: 'Dere har kommet dere inn i bøttekottet. Her henger det masse lyspærer som ikke lyser. Dere ser at det går en ledning fra lyspærene og til den tomme tavlen som henger over hvelvet. Kan det være en sammenheng mellom lyspærene og døren som leder til gullet?',
   3: 'Dere er nå i vaktbua. Siden det er juleferie har vakten tatt ferie, men han har ikke skrudd av datamaskinen sin. På maskinen lyser det opp et uforstålig ord og et tomt felt det går an å skrive inn i og en knapp. Resten av skjermen er tom. Det går en ledning fra maskinen inn til tavlen som henger over døren i hvelvet...',
-  4: 'Dere har åpnet luken til kjelleren. Her nede er alt nesten svart. det kan umulig være noen ledetråder i dette rommet? Dere ser at det går en ledning fra taket og opp mot tavlen over døren på hvelvet...',
+  4: 'Dere har åpnet luken til kjelleren. Her nede er alt nesten svart. det kan umulig være noen ledetråder i dette rommet? Dere ser det går en ledning fra taket og opp mot tavlen over døren på hvelvet...',
   5: 'side 5',
 }
