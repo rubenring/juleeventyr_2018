e7b0ecdf9a0fdb7637488e2f0f4d21d74f6dcb42
Reuben Ringdal <rubenringdal@gmail.com>
2018-12-10 20:16:04 +0100

    test5

--- Api/server.js
+++ Api/server.js
@@ -202,6 +202,10 @@ app.use((err, req, res, next) => {
   res.status(500).send(err.stack)
 })
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/frontend/build/index.html'));
+});
+
 app.listen(port, () => {
   db.then(() => {
     console.log('Connected correctly to mongo db')
